8072c8789247000b849457d64a9906ddc7685d20
Tiến anh Bùi <minhanhtienanh@gmail.com>
2021-04-14 10:48:54 +0700

    Add project files.

---
+++ All_JS/Controllers/Xoa1PhanTuRaKhoiMangController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace All_JS.Controllers
+{
+    public class Xoa1PhanTuRaKhoiMangController : Controller
+    {
+        // GET: Xoa1PhanTuRaKhoiMang
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
